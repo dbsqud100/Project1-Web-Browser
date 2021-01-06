323d90b7193d2bf5dbae7d4d1abd89087f6db258
dbsqud100 <67494551+dbsqud100@users.noreply.github.com>
2021-01-07 01:44:54 +0900

    Update main.js

--- main-screen/main.js
+++ main-screen/main.js
@@ -1,5 +1,6 @@
 const clockContainer = document.querySelector(".clock_div_cl");
 const clockTitle = clockContainer.querySelector(".clock_h1_cl");
+const body = document.querySelector("body");
 
 function getTime() {
     const date = new Date();
@@ -9,9 +10,25 @@ function getTime() {
     clockTitle.innerText = `${houre<10 ? `0${houre}`: houre} : ${minutes<10 ? `0${minutes}`: minutes} : ${second < 10 ? `0${second}` : second}`;
 }
 
-function init() {
+function paintBackground(number){
+    const image = new Image();
+    image.src = `image/${number+1}.jpg`;
+    image.classList.add("bgImage");
+    body.prepend(image)
+}
+
+function getRandom(){
+    const number = Math.floor(Math.random()*6);
+    return number;
+}
+
+function init(){
+    //clock
     getTime();
-    setInterval(getTime, 1000);
+    setInterval(getTime, 1000); 
+    //background
+    const random = getRandom();
+    paintBackground(random);
 }
 
 init();
